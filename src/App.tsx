dff4727aac658f7aae217d596f20a8bac7a555c9
John Carlo Dizon <dizon6171@gmail.com>
2025-02-06 23:08:58 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="name">
+      <h1>John Carlo Dizon</h1>
+      <div className="box">
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
